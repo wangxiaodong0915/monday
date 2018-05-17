5820e12404e43fba3cf3f53326d52fbbd25732a0
592901924@qq.com <592901924@qq.com>
2018-05-17 10:47:46 +0800

    create mondayMysql mondayTimer
    test mondayMysql mondayTimer

--- lib/mondayMysql.py
+++ lib/mondayMysql.py
@@ -33,13 +33,27 @@ class mondayMysql():
 
     def connect(self):
         try:
-            db = pymysql.connect(self.host, self.user, self.password, self.database, charset='utf8')  # charset='utf-8' 不然python3输出的是乱码
+            log.info("Mysql connect ……")
+            self.db = pymysql.connect(self.host, self.user, self.password, self.database, charset='utf8')  # charset='utf-8' 不然python3输出的是乱码
+            self.cursor = self.db.cursor()
+            log.info("Mysql connect to %s:%s by %s" % (self.host, self.database ,self.user))
         except Exception as e:
             log.error(e)
 
+    def execute(self,sql):
+        self.results = self.execute_silent(sql)
+        return self.results
     @func_timer
-    def execute(self):
-        pass
+    def execute_silent(self, sql):
+        try:
+            # 执行SQL语句
+            log.info("sql will be execute: %s" % sql)
+            self.cursor.execute(sql)
+            # 获取所有记录列表
+            self.results = self.cursor.fetchall()
+        except Exception as e:
+            log.error(e)
+        return self.results
 
     @func_timer
     def delete(self):
@@ -56,36 +70,7 @@ class mondayMysql():
     def useDatabase(self):
         pass
 
-#################################################################################
-log.info('search JOB msg from mysql ---- START ----')
-# 读取爬虫爬到的数据
-# 获取mysql信息
-# host = config.mysql['mysql_scrapy']['host']
-# user = config.mysql['mysql_scrapy']['user']
-# password = config.mysql['mysql_scrapy']['password']
-# database = config.mysql['mysql_scrapy']['database']
-# # 打开数据库连接
-# db = pymysql.connect(host, user,password, database, charset='utf8') # charset='utf-8' 不然python3输出的是乱码
-#
-# # 使用 cursor() 方法创建一个游标对象 cursor
-# cursor = db.cursor()
-#
-# # 获取数据的查询语句
-# sql = """select
-#  Positionname,
-#  Companyname,
-#  Workplace,
-#  Salary,
-#  Posttime,
-#  Experience
-#  from scrapy.qcwy"""
-#
-# try:
-#     # 执行SQL语句
-#     cursor.execute(sql)
-#     # 获取所有记录列表
-#     results = cursor.fetchall()
-#     log.info('search JOB msg from mysql ---- END ----')
-# except Exception as e:
-#     log.error("unable to fetch data")
-#     log.error(e)
+    def __exit__(self):
+        self.cursor.close()
+        self.db.close()
+        log.info('Mysql connect closed!')
---
+++ testjob/mysqltest.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+# !/usr/bin/python3
+# @version: V
+# @author: 飞翔的卡夫卡
+# @contact: 592901924@qq.com
+# @site: 
+# @software: PyCharm
+# @file: mysqltest.py
+# @time: 2018/5/17 9:56
+# @python：python3.6
+
+import config
+import mondayMysql
+#导入封装的日志记录模块
+from mondayLog import MondayLogger
+log = MondayLogger('mysqltest')
+
+# Mysql login infomation
+host = config.mysql['mysql_scrapy']['host']
+user = config.mysql['mysql_scrapy']['user']
+password = config.mysql['mysql_scrapy']['password']
+database = config.mysql['mysql_scrapy']['database']
+log.info("Mysql connect to %s:%s by %s" % (host, database, user) )
+
+# Mysql connect and execute  a query sql
+conn = mondayMysql.mondayMysql(host, user, password, database)
+conn.connect()
+log.info('login successful!')
+
+#select sql
+sql = """
+SELECT 
+positionname
+FROM scrapy.qcwy
+"""
+
+#execute sql
+results  = conn.execute(sql)
+
+# print(results)
+for row in results:
+    print(row[0])
