c06f60279dbe602b54820c0aff022f085220d441
王晓东 <592901924@qq.com>
2018-05-22 00:25:58 +0800

    commit env file

--- script/pistatussend.py
+++ script/pistatussend.py
@@ -1,5 +1,5 @@
-# -*- coding: UTF-8 -*-
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 # @version: V
 # @author: 飞翔的卡夫卡
 # @contact: 592901924@qq.com
@@ -8,50 +8,6 @@
 # @file: pistatussend.py
 # @time: 2018/5/21 10:03
 # @python：python3.6
-
-# import psutil
-# import datetime
-
-'''  
-#查看cpu的信息  
-print u"CPU 个数 %s"%psutil.cpu_count()  
-print u"物理CPU个数 %s"%psutil.cpu_count(logical=False)  
-print u"CPU uptimes"  
-print psutil.cpu_times()  
-print ""  
-
-#查看内存信息  
-# print u"系统总内存 %s M"%(psutil.TOTAL_PHYMEM/1024/1024)  
-# print u"系统可用内存 %s M"%(psutil.avail_phymem()/1024/1024)  
-# mem_rate = int(psutil.avail_phymem())/float(psutil.TOTAL_PHYMEM)  
-# print u"系统内存使用率 %s %%"%int(mem_rate*100)  
-
-#系统启动时间  
-print u"系统启动时间 %s"%datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")  
-
-#系统用户  
-users_count = len(psutil.users())  
-users_list = ",".join([ u.name for u in psutil.users()])  
-print u"当前有%s个用户，分别是%s"%(users_count, users_list)  
-
-#网卡，可以得到网卡属性，连接数，当前流量等信息  
-net = psutil.net_io_counters()  
-bytes_sent = '{0:.2f} kb'.format(net.bytes_recv / 1024)  
-bytes_rcvd = '{0:.2f} kb'.format(net.bytes_sent / 1024)  
-print u"网卡接收流量 %s 网卡发送流量 %s"%(bytes_rcvd, bytes_sent)  
-'''
-
-#进程  进程的各种详细参数
-# # 查看系统全部进程
-# print(u"系统全部进程 %s"%psutil.pids())
-# for pnum in psutil.pids():
-#     p = psutil.Process(pnum)
-#     print(u"进程名 %-20s  内存利用率 %-18s 进程状态 %-10s 创建时间 %-10s "%(p.name(),p.memory_percent(),p.status(),p.create_time()))
-#     # print p.io_counters()    #进程读写信息
-# p.cwd()    #进程的工作目录绝对路径
-# p.status()   #进程状态
-# p.create_time()  #进程创建时间
-# p.uids()    #进程uid信息
 
 from datetime import date
 
